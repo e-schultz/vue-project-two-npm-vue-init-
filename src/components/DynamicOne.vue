0806b6ab6807278f6ccd690065813ca5001d129a
e-schultz <evan@schultz.codes>
2022-01-26 20:03:34 -0500

    dynamic swap test

---
+++ src/components/DynamicOne.vue
@@ -0,0 +1,13 @@
+<script setup>
+const props = defineProps({
+  message: String,
+});
+</script>
+<template>
+  <h1>{{ message }}</h1>
+</template>
+<style scoped>
+h1 {
+  border: 1px solid black;
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,29 @@
+<script setup>
+import { ref } from "vue";
+import DynamicOne from "../components/DynamicOne.vue";
+import DynamicTwo from "../components/DynamicTwo.vue";
+// these are available to access within the template
+const aboutTitle = "Welcome to the Workshop";
+const buttonText = "Click Me";
+const showWhich = ref("DynamicOne");
+const components = {
+  DynamicOne,
+  DynamicTwo,
+};
+</script>
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{ aboutTitle }}</h1>
+    <p>
+      <button @click="showWhich = 'DynamicOne'">Show One</button> |
+      <button @click="showWhich = 'DynamicTwo'">Show Two</button>
+    </p>
+    <p>
+      <DynamicTwo :message="aboutTitle" />
+    </p>
+    <h2>Swap Components Dynamically</h2>
+    <component :is="components[showWhich]" :message="aboutTitle"></component>
+    <p></p>
   </div>
 </template>
 
